fdf65ee442dd3d0535e1fcce80541672a11ec208
Vic Amuso <vic@parablesoft.com>
2016-07-29 16:22:29 -0400

    Re-org of stylesheets into their own folder

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ module.exports = {
 		app.import("bower_components/font-awesome/css/font-awesome.css");
 		app.import("bower_components/sweetalert/dist/sweetalert.css");
 		app.import("bower_components/sweetalert/dist/sweetalert-dev.js");
-    app.import("vendor/fade-in.css");
-    app.import("vendor/blank-slate.css");
-    app.import("vendor/page-header.css");
-    app.import("vendor/generic.css");
+    app.import("vendor/assets/stylesheets/fade-in.css");
+    app.import("vendor/assets/stylesheets/blank-slate.css");
+    app.import("vendor/assets/stylesheets/page-header.css");
+    app.import("vendor/assets/stylesheets/generic.css");
     app.import("vendor/assets/sounds/errorNotification.mp3");
 	}
 };
