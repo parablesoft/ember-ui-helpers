ef1992be618e303d86d7401a5b91dbc78db2d940
Vic Amuso <vic@parablesoft.com>
2016-07-29 16:59:23 -0400

    Added option to decline fade-in on loading message

--- addon/components/loading-message.js
+++ addon/components/loading-message.js
@@ -8,5 +8,7 @@ const {Component} = Ember;
 export default Component.extend({
 	layout: layout,
 	message: "Loading ...",
-	classNames: ["ember-ui-helpers-loading-message fade-in"],
+	classNames: ["ember-ui-helpers-loading-message"],
+	useFadeIn: true,
+	classNameBindings: ["useFadeIn:fade-in"]
 });
