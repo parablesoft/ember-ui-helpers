ed5c9668ec841f35584f3587961925bf5aa4c997
Vic Amuso <vic@parablesoft.com>
2016-07-29 11:10:09 -0400

    Blank Slate updates

--- addon/components/blank-slate.js
+++ addon/components/blank-slate.js
@@ -1,11 +1,16 @@
 import Ember from 'ember';
-import layout from '../templates/components/loading-message';
+import layout from '../templates/components/blank-slate';
+import {singularize, pluralize} from 'ember-inflector';
 
 
-const {Component} = Ember;
+const {on,get,Component,computed,isEmpty} = Ember;
 
 
 export default Component.extend({
 	layout: layout,
-	classNames: ["ember-ui-helpers-blank-slate"],
+	classNames: ["ember-ui-helpers-blank-slate text-center"],
+	resources: computed("resource",function(){
+		return pluralize(get(this,"resource"));
+	}),
+	buttonClass: "btn btn-default"
 });
