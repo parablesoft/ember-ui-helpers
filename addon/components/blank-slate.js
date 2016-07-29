05d00a2f1202a017a7384ddafad43eaf565ed49f
Vic Amuso <vic@parablesoft.com>
2016-07-29 16:50:54 -0400

    Removed pluralizer

--- addon/components/blank-slate.js
+++ addon/components/blank-slate.js
@@ -9,8 +9,5 @@ const {on,get,Component,computed,isEmpty} = Ember;
 export default Component.extend({
 	layout: layout,
 	classNames: ["ember-ui-helpers-blank-slate text-center"],
-	resources: computed("resource",function(){
-		return pluralize(get(this,"resource"));
-	}),
 	buttonClass: "btn btn-default"
 });
