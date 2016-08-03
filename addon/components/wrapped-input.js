d70823fdc964af5f319eb63f8e05be32353712fc
Vic Amuso <vic@parablesoft.com>
2016-08-03 06:39:05 -0400

    Added auto-focus-delay to wrapped input

--- addon/components/wrapped-input.js
+++ addon/components/wrapped-input.js
@@ -6,6 +6,7 @@ const {Component,isPresent,computed} = Ember;
 export default Component.extend({
   layout: layout,
 	classNames: ["form-group"],
+	autoFocusDelay:null,
 	placeholderText: computed("placeholder","labelText",function(){
 		let {placeholder,labelText} = this.getProperties("placeholder","labelText");
 		return isPresent(placeholder) ? placeholder : labelText;
