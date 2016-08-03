3352d4d4feafd8b377a478ed19a1e9bb79f7f00f
Vic Amuso <vic@parablesoft.com>
2016-08-03 06:35:53 -0400

    Added delay to auto-focus controls

--- addon/mixins/auto-focus-control.js
+++ addon/mixins/auto-focus-control.js
@@ -2,11 +2,23 @@ import Ember from 'ember';
 
 const {Mixin,get,observer,} = Ember;
 export default Mixin.create({
+	delay: null,
 	didInsertElement(){
 		let doNotFocus = get(this,"doNotFocus");
 		if(!doNotFocus){
-			this.$().focus();
+			let delay = get(this,"delay");
+			if(delay!=null){
+				Ember.run.later(this,function(){
+					this.focusMe();
+				},delay);
+			}
+			else{
+				this.focusMe();
+			}
 		}
+	},
+	focusMe(){
+		this.$().focus();
 	},
 	refocus: observer("value", function(){
 		let value = get(this,"value");
