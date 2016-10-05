227eb99005ab3be7f944488de1faa4e6623d47dd
Vic Amuso <vic@parablesoft.com>
2016-10-05 06:54:26 -0400

    Removed ember-computed-decorators

--- index.js
+++ index.js
@@ -4,18 +4,6 @@
 module.exports = {
   name: 'ember-ui-helpers',
 	included: function(app){
-
-		this._super.init && this._super.init.apply(this, arguments);
-
-		this.options = this.options || {};
-		this.options.babel = this.options.babel || {};
-		this.options.babel.optional = this.options.babel.optional || [];
-
-		if (this.options.babel.optional.indexOf('es7.decorators') === -1) {
-			this.options.babel.optional.push('es7.decorators');
-		}
-
-
 		app.import("bower_components/font-awesome/css/font-awesome.css");
 		app.import("bower_components/sweetalert/dist/sweetalert.css");
 		app.import("bower_components/sweetalert/dist/sweetalert-dev.js");
