f81bb0cf587689491f3f1be9daeaa18d2084762a
Vic Amuso <vic@parablesoft.com>
2016-12-07 09:40:36 -0500

    Fix typo, bump version

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ module.exports = {
   included: function(app){
     // app.import("bower_components/font-awesome/css/font-awesome.css");
     app.import("vendor/assets/sweetalert/dist/sweetalert.css");
-    app.import("vendors/assets/sweetalert/dist/sweetalert-dev.js");
+    app.import("vendor/assets/sweetalert/dist/sweetalert-dev.js");
     app.import("vendor/assets/stylesheets/fade-in.css");
     app.import("vendor/assets/stylesheets/blank-slate.css");
     app.import("vendor/assets/stylesheets/page-header.css");
