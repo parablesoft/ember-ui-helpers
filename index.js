d2546e1bc567e1a221213c240b54f2ecbfb57880
Vic Amuso <vic@parablesoft.com>
2016-12-07 09:32:30 -0500

    Bump version, small change in dummy

--- index.js
+++ index.js
@@ -3,14 +3,14 @@
 
 module.exports = {
   name: 'ember-ui-helpers',
-	included: function(app){
-		app.import("bower_components/font-awesome/css/font-awesome.css");
-		app.import("bower_components/sweetalert/dist/sweetalert.css");
-		app.import("bower_components/sweetalert/dist/sweetalert-dev.js");
+  included: function(app){
+    app.import("bower_components/font-awesome/css/font-awesome.css");
+    app.import("bower_components/sweetalert/dist/sweetalert.css");
+    app.import("bower_components/sweetalert/dist/sweetalert-dev.js");
     app.import("vendor/assets/stylesheets/fade-in.css");
     app.import("vendor/assets/stylesheets/blank-slate.css");
     app.import("vendor/assets/stylesheets/page-header.css");
     app.import("vendor/assets/stylesheets/generic.css");
     app.import("vendor/assets/sounds/errorNotification.mp3");
-	}
+  }
 };
