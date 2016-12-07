e480721aded59c1feb6ed12a9fef769239f07db7
Vic Amuso <vic@parablesoft.com>
2016-12-07 09:39:37 -0500

    Fix to sweet alert

--- index.js
+++ index.js
@@ -4,9 +4,9 @@
 module.exports = {
   name: 'ember-ui-helpers',
   included: function(app){
-    app.import("bower_components/font-awesome/css/font-awesome.css");
-    app.import("bower_components/sweetalert/dist/sweetalert.css");
-    app.import("bower_components/sweetalert/dist/sweetalert-dev.js");
+    // app.import("bower_components/font-awesome/css/font-awesome.css");
+    app.import("vendor/assets/sweetalert/dist/sweetalert.css");
+    app.import("vendors/assets/sweetalert/dist/sweetalert-dev.js");
     app.import("vendor/assets/stylesheets/fade-in.css");
     app.import("vendor/assets/stylesheets/blank-slate.css");
     app.import("vendor/assets/stylesheets/page-header.css");
